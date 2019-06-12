90f9b5d7052902a65bb24b074176ec759a5b0df6
Dmitrii Gushchin <Dmitrii_Gushchin@epam.com>
2019-06-12 23:38:08 +0300

    Task 01. Basic types

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,51 @@ function showHello(divName: string, name: string) {
   const elt = document.getElementById(divName);
   elt.innerText = `Hello from ${name}`;
 }
+
+// =======================================================
+
+function getAllBooks(): object[] {
+  let books: Array<object> = [  
+    { 
+      title: 'Refactoring JavaScript',
+      author: 'Evan Burchard',
+      available: true
+    },
+    { 
+      title: 'JavaScript Testing',
+      author: 'Liang Yuxian Eugene',
+      available: false
+    },
+    { 
+      title: 'CSS Secrets',
+      author: 'Lea Verou',
+      available: true },
+    { 
+      title: 'Mastering JavaScript Object-Oriented Programming',
+      author: 'Andrea Chiarelli',
+      available: true
+    }
+  ];
+
+  return books;
+}
+
+function logFirstAvailable(books: any[]): void {
+  const numberOfBooks: number = books.length;
+  let firstAvailableTitle: string = '';
+
+  for(const book of books) {
+    if (book.available) {
+      firstAvailableTitle = book.title;
+      break;
+    }
+  }
+
+  console.log(`total books: ${numberOfBooks}`);
+  console.log(`first available book title: ${firstAvailableTitle}`);
+}
+
+// =======================================================
+
+// Task 01
+logFirstAvailable(getAllBooks());
